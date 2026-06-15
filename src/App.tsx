db48f77b3dd50bdeba5e5788e07de5bed8471b62
adsuper04214-droid <adsuper04214@gmail.com>
2026-06-16 01:36:44 +0900

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -235,7 +235,7 @@ export default function App() {
                       <img 
                         src={profilePhoto} 
                         alt="김도현 프로필" 
-                        className="w-full h-full object-contain object-bottom scale-[1.3] origin-bottom select-none" 
+                        className="w-full h-full object-contain object-bottom scale-[1.05] origin-bottom select-none" 
                       />
                     </div>
                   </div>
